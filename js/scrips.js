1f8200d4011ccf1116c2b4094874feb9f8524459
WolfBeil <84537202+WolfBeil@users.noreply.github.com>
2025-03-03 20:08:05 -0400

    Declare playGame function
    Inside the playGame function a for loop was made.
    In that said loop, the playRound function was declared.
    Now, the number of rounds to be played is going to be
    qual to the 'roduns' argument given to the playGame function.

--- js/scrips.js
+++ js/scrips.js
@@ -1,5 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice(choices) {
     let computerChoice = Math.floor(Math.random() * choices);
@@ -37,33 +35,48 @@ function getHumanChoice() {
     }
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice(3);
+let humanScore = 0;
+let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!")
-        return "Tie";
-       } else if (
-            (humanChoice === "Rock") && (computerChoice === "Scissors") ||
-            (humanChoice === "Paper") && (computerChoice === "Rock") ||
-            (humanChoice === "Scissors") && (computerChoice === "Paper") ) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++;
-            return "Player wins";
+// The rounds argument will dictate how many rounds will be played
+
+function playGame(rounds) {
+
+    for (let index = 0; index < rounds; index++) {
+
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice(3);
+
+        function playRound(humanChoice, computerChoice) {
+            if (humanChoice === computerChoice) {
+                console.log("It's a tie!")
+                return "Tie";
+               } else if (
+                    (humanChoice === "Rock") && (computerChoice === "Scissors") ||
+                    (humanChoice === "Paper") && (computerChoice === "Rock") ||
+                    (humanChoice === "Scissors") && (computerChoice === "Paper") ) {
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                    humanScore++;
+                    return "Player wins";
+                }
+                else if (
+                    (computerChoice === "Paper") && (humanChoice === "Rock") ||
+                    (computerChoice === "Rock") && (humanChoice === "Scissors") ||
+                    (computerChoice === "Scissors") && (humanChoice === "Paper") ) {
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                    computerScore++;
+                    return "Player loses";
+                }
+                else {
+                    console.log("An error must have ocurred")
+                    return "Error";
+               }
         }
-        else if (
-            (computerChoice === "Paper") && (humanChoice === "Rock") ||
-            (computerChoice === "Rock") && (humanChoice === "Scissors") ||
-            (computerChoice === "Scissors") && (humanChoice === "Paper") ) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++;
-            return "Player loses";
-        }
-        else {
-            console.log("An error must have ocurred")
-            return "Error";
-       }
+        
+        playRound(humanSelection, computerSelection);
+
+    }
 }
 
-playRound(humanSelection, computerSelection);
+playGame(5);
+console.log(`Player Score: ${humanScore} --- Computer Score: ${computerScore}`);
